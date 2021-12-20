2935f3892c6fb623445e9f5a4b8c01ca714d18db
ChenFangM <Fang1113848389@gmail.com>
2021-12-20 11:43:57 -0500

    Runnable Code

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ Roster: Fang Min Chen, Kaitlin Ho, Jing Yi Feng
 		Duckies: Cat, Biscuit, Apple
 
 ## Results
+Ran Driver:
+Average run time for Binary Search is 0.00125
+Average run time for Linear Search is 0.00625
 
 ## Conclusions
+Binary Search is faster on average when searching in arrays of length 4000, for targets WITHIN the array
--- SearchDriver.java
+++ SearchDriver.java
@@ -1,16 +1,59 @@
 public class SearchDriver {
 
+	public static double mean(long[] a) {
+		long sum = 0;
+		for (int i = 0; i < a.length; i ++) {
+			sum += a[i]; 
+		}
+		return ((double) sum) / a.length;
+	}
+
+	public static int binSearch(Comparable[] a, Comparable target) {
+		return BinSearch.binSearch(a, target); 
+	}
+
+	public static int linSearch(Comparable[] a, Comparable target) {
+		return LinSearch.linSearch(a, target);
+	}
+
+
 	public static void main(String[] args) {
 
-		// Testing currentTimeMillis()
-		System.out.println(System.currentTimeMillis());
-		System.out.println("Item 1");
-		System.out.println(System.currentTimeMillis());
-		for (int i = 2; i < 16; i ++) {
-			System.out.println("Item " + i);
-		}
-		System.out.println(System.currentTimeMillis());
+		Comparable[] arr = new Comparable[4000];
 
+		// Populate array in ascending order
+		for (int i = 0; i < arr.length; i ++) {
+			arr[i] = i * 2;
+		}
+
+		// Array with all the time differences
+		long[] timesBin = new long[arr.length];
+		long[] timesLin = new long[arr.length];
+
+		// Temporary Variables
+		long sTime = 0;
+		long eTime = 0;
+		int foundIndex = 0;
+
+		// Test average time to Binary Search
+		for (int i = 0; i < arr.length; i ++) {
+			sTime = System.currentTimeMillis();
+			foundIndex = binSearch(arr, arr[i]);
+			eTime = System.currentTimeMillis();
+			timesBin[i] = eTime - sTime;
+		}
+
+		System.out.println(mean(timesBin));
+
+		for (int i = 0; i < arr.length; i ++) {
+			sTime = System.currentTimeMillis();
+			foundIndex = linSearch(arr, arr[i]);
+			eTime = System.currentTimeMillis();
+			timesLin[i] = eTime - sTime;
+		} 
+
+		System.out.println(mean(timesLin));
+		
 	}
 
 }
