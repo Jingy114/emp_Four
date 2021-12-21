6fe68c3b06c2580e36a0b8c0ea0c72ca7b4471db
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-12-20 21:33:44 -0500

    Update SearchDriver2.java

--- SearchDriver2.java
+++ SearchDriver2.java
@@ -17,100 +17,66 @@ public class SearchDriver2 {
 	}
 
   public static long run(Comparable[] arr){
-    // Array with all the time differences
-    //long[] timesBin = new long[arr.length];
-    //long[] timesLin = new long[arr.length];
+	// Populate array in ascending order
+	for (int i = 0; i < arr.length; i ++) {
+		arr[i] = i * 2;
+	}
 
-    long binTime;
-    long linTime;
+	// Temporary Variables
+	long sTime = 0;
+	long eTime = 0;
+	int foundIndex = 0;
 
-    // Temporary Variables
-    long sTime = 0;
-    long eTime = 0;
-    int foundIndex = 0;
+	// Test average time to Binary Search
+	sTime = System.currentTimeMillis();
+	for (int i = 0; i < arr.length; i ++) {
+		//sTime = System.currentTimeMillis();
+		foundIndex = binSearch(arr, arr[i]);
+		//eTime = System.currentTimeMillis();
+		//timesBin[i] = eTime - sTime;
+	}
+	eTime = System.currentTimeMillis();
 
-    // Test average time to Binary Search
-    sTime = System.currentTimeMillis();
-    for (int i = 0; i < arr.length; i ++) {
-      //sTime = System.currentTimeMillis();
-      foundIndex = binSearch(arr, arr[i]);
-      //eTime = System.currentTimeMillis();
-      //timesBin[i] = eTime - sTime;
-    }
-	  
-    eTime = System.currentTimeMillis();
+	System.out.println(eTime - sTime);
 
-    binTime = eTime - sTime;
-    //System.out.println(binTime);
+	//System.out.println(mean(timesBin));
 
-    //System.out.println(mean(timesBin));
-
-    // Test avg time to Linear Search
-    sTime = System.currentTimeMillis();
-    for (int i = 0; i < arr.length; i ++) {
-      //sTime = System.currentTimeMillis();
-      foundIndex = linSearch(arr, arr[i]);
-      //eTime = System.currentTimeMillis();
-      //timesLin[i] = eTime - sTime;
-    }
-    eTime = System.currentTimeMillis();
-    linTime = eTime - sTime;
-    //System.out.println(linTime);
-    //System.out.println(mean(timesLin));
-
-    return binTime - linTime;
+	// Test avg time to Linear Search
+	sTime = System.currentTimeMillis();
+	for (int i = 0; i < arr.length; i ++) {
+		//sTime = System.currentTimeMillis();
+		foundIndex = linSearch(arr, arr[i]);
+		//eTime = System.currentTimeMillis();
+		//timesLin[i] = eTime - sTime;
+	} 
+	eTime = System.currentTimeMillis();
+	System.out.println(eTime - sTime);
+	//System.out.println(mean(timesLin));
 
   }
 
 
 	public static void main(String[] args){
 	
+	System.out.println("-----------thousand-----------");
 	Comparable[] thousand = new Comparable[1000];
-	System.out.println(run(thousand));
+	run(thousand);
 		
-	Comparable[] tenT = new Comparable[10000];
-	System.out.println(run(thousand));
+// 	Comparable[] tenT = new Comparable[10000];
+// 	System.out.println(run(thousand));
 		
-	Comparable[] hundredT = new Comparable[100000];
-	System.out.println(run(thousand));
+// 	Comparable[] hundredT = new Comparable[100000];
+// 	System.out.println(run(thousand));
 	
-	Comparable[] fiveHT = new Comparable[500000];
-	System.out.println(run(thousand));
+// 	Comparable[] fiveHT = new Comparable[500000];
+// 	System.out.println(run(thousand));
 	
-	Comparable[] mil = new Comparable[1000000];
-	System.out.println(run(thousand));
+// 	Comparable[] mil = new Comparable[1000000];
+// 	System.out.println(run(thousand));
 	
-	Comparable[] fiveM = new Comparable[5000000];
-	System.out.println(run(thousand));
+// 	Comparable[] fiveM = new Comparable[5000000];
+// 	System.out.println(run(thousand));
 	
-// 	long[] binVlin = new long[50];
-// 	int n = 1000;
-// 	for (int i = 0; i < 50; i++){
-// 		Comparable[] arr = new Comparable[n];
-// 		//binVlin[i] = run(arr);
-
-// 		System.out.print(run(arr));
-// 		n = (int) Math.pow(1000, i);
-// 	}
-	
-// 	for (long i = 0; i < 50; i++){
-// 		long result = binVlin[i];
-// 		System.out.print(result + " ");	
-// 	}
-	
-		
-		
-//     while (run(arr) > 0){
-//     	Comparable[] arr = new Comparable[n];
-//       // Populate array in ascending order
-//       for (int i = 0; i < arr.length; i ++) {
-//         arr[i] = i * 2;
-//       }
-
-//       run(arr);
-
-//     }
-
 
 	}
 
